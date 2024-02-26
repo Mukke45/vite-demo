fb796eb7617216a80f2bb1cc4b9bfa20b124d841
Mukhtaar Bade <mukhtaar@Mukhtaar-MacBook-Air.local>
2024-02-26 13:58:38 +0200

    tehtavat

--- main.js
+++
@@ -1,9 +0,0 @@
-import './style.css'
-import './minun-style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
-
-document.querySelector('#app').innerHTML = `Moi täällä ollaan`
-
-setupCounter(document.querySelector('#counter'))
